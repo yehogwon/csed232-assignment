d531a16e8f6f8eb4f925b05c8e71f84cdcb94e27
Yeho Gwon <devstita@gmail.com>
2023-03-10 20:51:57 +0900

    added lines of comments

--- assn1/prob1_20220358/main.cpp
+++ assn1/prob1_20220358/main.cpp
@@ -1,22 +1,34 @@
+/*
+ * Author: Yeho Gwon (20220358)
+ * Date: 2023/03/10
+ * ASSN1, Prob #1
+ */
+
 #include <iostream>
 
+/**
+ * @brief print the number `n` in the base of `base`
+ * @param n: integer number to be converted
+ * @param base: base of the conversion
+ * @return none
+*/
 void convert(int n, int base) {
-    if (n == 0) return;
-    convert(n / base, base);
+    if (n == 0) return; // exit condition (the conversion has ended)
+    convert(n / base, base); // call the function recursively
 
-    int rest = n % base;
+    int rest = n % base; // current digit
     if (rest < 10)
-        std::cout << rest;
+        std::cout << rest; // print the digit as it is
     else
-        std::cout << (char) ('A' + rest - 10);
+        std::cout << (char) ('A' + rest - 10); // print the digit as a character
 }
 
 int main() {
-    int num, base;
-    std::cin >> num >> base;
+    int num, base; // num: integer number to be converted, base: base (진법) of the conversion
+    std::cin >> num >> base; // get the two numbers from the user
     
-    convert(num, base);
+    convert(num, base); // call convert() to convert the 
     std::cout << std::endl;
 
-    return 0;
+    return 0; // Successfully terminated
 }
