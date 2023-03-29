0c73402f07c979155a374701efd27f617f64a2bf
Yeho Gwon <devstita@gmail.com>
2023-03-29 20:20:25 +0900

    cleaned io.hpp

--- assn2/src/io.hpp
+++ assn2/src/io.hpp
@@ -17,11 +17,6 @@ bool no_space(const std::string &str);
 bool no_lower(const std::string &str);
 
 void input_int(std::istream &is, std::ostream &os, const char *prompt, int &i, CheckInt check);
-
 void input_str(std::istream &is, std::ostream &os, const char *prompt, std::string &str, CheckStr check);
-
-// void input_without_whitespace(const std::istream &is, const std::ostream &os, const T &prompt, std::string &str);
-
-// void input_only_upper(const std::istream &is, const std::ostream &os, const T &prompt, std::string &str);
 
 #endif
