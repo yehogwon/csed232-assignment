01352a5de07d8b8cb3d323360cbcd43af6a56c0c
Yeho Gwon <devstita@gmail.com>
2023-03-30 11:08:28 +0900

    updated an acess control of Format

--- assn2/src/io.hpp
+++ assn2/src/io.hpp
@@ -5,20 +5,21 @@
 #include <string>
 
 class Format { // public comes first following ios convention
-public: // TODO: What about use struct instead of class?
+public:
     typedef unsigned int check_bit;
 
     static const check_bit GENDER = 0x1;
     static const check_bit NOSPACE = 0x2;
     static const check_bit NOLOWER = 0x4;
 
+    static void strict_input(std::istream &is, std::ostream &os, const char *prompt, std::string &str, check_bit check);
+    static void range_input(std::istream &is, std::ostream &os, const char *prompt, int &i, int min, int max); // inclusive
+
+private: 
     static bool gender(std::string &str);
     static bool nospace(std::string &str);
     static bool nolower(std::string &str);
     static bool all(std::string &str, check_bit check);
-
-    static void strict_input(std::istream &is, std::ostream &os, const char *prompt, std::string &str, check_bit check);
-    static void range_input(std::istream &is, std::ostream &os, const char *prompt, int &i, int min, int max); // inclusive
 };
 
 void clear_buffer(std::istream &is);
