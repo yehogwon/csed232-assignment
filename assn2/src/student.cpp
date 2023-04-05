85f5af39db44067e8b7c0152f6ec575fce6e3901
Yeho Gwon <devstita@gmail.com>
2023-04-05 22:41:42 +0900

    added some comments on student*

--- assn2/src/student.cpp
+++ assn2/src/student.cpp
@@ -12,9 +12,14 @@
 Student::Student() : dept(""), gender(""), name(""), age(0) { }
 
 Student::Student(std::istream &is, std::ostream &os) {
+    // get input from the user under the following conditions:
+    // (1) dept should not contain space and should consist only of uppercase. Also, it should not be empty. 
     Format::strict_input(is, os, "Dept: ", dept, Format::NOSPACE | Format::NOLOWER | Format::NOEMPTY);
+    // (2) gender should either "M", "F". So, it should not contain whitespace and be empty.
     Format::strict_input(is, os, "Gender: ", gender, Format::NOSPACE | Format::GENDER | Format::NOEMPTY);
+    // (3) name should not contain whitespace and be empty. 
     Format::strict_input(is, os, "Name: ", name, Format::NOSPACE | Format::NOEMPTY);
+    // (4) age should be a integer and within the range of MIN_AGE and MAX_AGE (inclusive)
     Format::range_input(is, os, "Age: ", age, MIN_AGE, MAX_AGE);
 }
 
@@ -31,18 +36,20 @@ int Student::get_age() const {
 }
 
 bool comp_dept(const Student &s1, const Student &s2) {
-    return s1.dept >= s2.dept;
+    return s1.dept >= s2.dept; // compare dept of s1 and s2
 }
 
 bool comp_gender(const Student &s1, const Student &s2) {
-    return s1.gender >= s2.gender;
+    return s1.gender >= s2.gender; // compare gender of s1 and s2
 }
 
 bool operator==(const Student &s1, const Student &s2) {
+    // compare every member variables of s1 and s2
     return s1.dept == s2.dept && s1.gender == s2.gender && s1.name == s2.name && s1.age == s2.age;
 }
 
 bool operator>(const Student &s1, const Student &s2) {
+    // the order of comparison is dept > gender > name > age
     if (s1.dept != s2.dept) return s1.dept > s2.dept;
     else if (s1.gender != s2.gender) return s1.gender > s2.gender;
     else if (s1.name != s2.name) return s1.name > s2.name;
@@ -50,10 +57,11 @@ bool operator>(const Student &s1, const Student &s2) {
 }
 
 bool operator<(const Student &s1, const Student &s2) {
-    return !(s1 > s2) && !(s1 == s2);
+    return !(s1 > s2) && !(s1 == s2); // trichotomy property
 }
 
 std::ostream& operator<<(std::ostream &os, const Student &s) {
+    // print dept, gender, name, age separated by tab
     os << s.dept << "\t" << s.gender << "\t" << s.name << "\t" << s.age;
     return os;
 }
--- assn2/src/student.hpp
+++ assn2/src/student.hpp
@@ -11,8 +11,8 @@
 #include <string>
 #include "io.hpp"
 
-#define MIN_AGE 18
-#define MAX_AGE 30
+#define MIN_AGE 18 // minimum age
+#define MAX_AGE 30 // maximum age
 
 class Student {
 private: 
@@ -21,13 +21,39 @@ private:
     int age;
 
 public: 
+    /**
+     * @brief default constructor (set std::string variables to "" (empty string) and int variable to 0)
+     */
     Student();
+
+    /**
+     * @brief constructor to get student information from the user
+     * @param is input stream to get student information
+     * @param os output stream to show the user what to input (prompt)
+     */
     Student(std::istream &is, std::ostream &os);
 
+    // TODO: Change the return type to const std::string& (reference)
+
+    /**
+     * @brief get department (getter; data hiding)
+     * @return dept
+     */
     std::string get_dept() const;
+
+    /**
+     * @brief get gender (getter; data hiding)
+     * @return gender
+     */
     std::string get_gender() const;
+
+    /**
+     * @brief get age (getter; data hiding)
+     * @return age
+     */
     int get_age() const;
 
+    // Functions below are friend functions because they need to access private members of Student class (dept, gender, name, age)
     friend bool comp_dept(const Student &s1, const Student &s2);
     friend bool comp_gender(const Student &s1, const Student &s2);
 
@@ -36,12 +62,47 @@ public:
     friend std::ostream& operator<<(std::ostream &os, const Student &s);
 };
 
+
+/**
+ * @brief compare two Student objects by dept
+ * @param s1 operand 1
+ * @param s2 operand 2
+ * @return true if s1.dept >= s2.dept, false otherwise
+ */
 bool comp_dept(const Student &s1, const Student &s2);
+
+/**
+ * @brief compare two Student objects by gender
+ * @param s1 operand 1
+ * @param s2 operand 2
+ * @return true if s1.gender >= s2.gender, false otherwise
+ */
 bool comp_gender(const Student &s1, const Student &s2);
 
+/**
+ * @brief compare two Student objects (overloading operator==)
+ * @param s1 operand 1
+ * @param s2 operand 2
+ * @return true if all members of s1 and s2 are equal, false otherwise
+ */
 bool operator==(const Student &s1, const Student &s2);
+
+/**
+ * @brief compare two Student objects (overloading operator>)
+ * @param s1 operand 1
+ * @param s2 operand 2
+ * @return true if s1 > s2 (the order of comparison: dept, gender, name, age)
+ */
 bool operator>(const Student &s1, const Student &s2);
+
+/**
+ * @brief compare two Student objects (overloading operator<)
+ * @param s1 operand 1
+ * @param s2 operand 2
+ * @return true if s1 < s2 (the order of comparison: dept, gender, name, age)
+ */
 bool operator<(const Student &s1, const Student &s2);
-std::ostream& operator<<(std::ostream &os, const Student &s);
+
+std::ostream& operator<<(std::ostream &os, const Student &s); // print Student object (overloading operator<<)
 
 #endif
