1a6663032fbd2b1c9685a2762557af107b031bb6
Yeho Gwon <devstita@gmail.com>
2023-04-06 20:44:13 +0900

    added some lines of comments on list*

--- assn2/src/list.cpp
+++ assn2/src/list.cpp
@@ -1,29 +1,29 @@
 /*
  * CSED232 ASSN2
  * Author: Yeho Gwon (20220358)
- * Date: 2023/03/05
+ * Date: 2023/03/06
  * File Name: list.cpp
  */
 
 #include "list.hpp"
 
 List::List() {
-    head = new Node(Student());
-    for (int i = 0; i < MAX_DEPT; i++)
-        dept_status[i] = {"", 0};
+    head = new Node(Student()); // create a dummy node
+    for (int i = 0; i < MAX_DEPT; i++) // initialize the departments array
+        dept_status[i] = {"", 0}; // empty string and 0
 }
 
 List::~List() {
-    Node *cur = head;
+    Node *cur = head; // start from the dummy node
     while (cur != nullptr) {
-        Node *tmp = cur;
-        cur = cur->next;
-        delete tmp;
+        Node *tmp = cur; // temporarily save the current node
+        cur = cur->next; // move on to the next node
+        delete tmp; // free the memory of the current node
     }
 }
 
 std::string List::to_string(Operator op) const {
-    switch (op) {
+    switch (op) { // return the name of the corresponding operator
         case AVG: return "Average";
         case MAX: return "Max";
         case MIN: return "Min";
@@ -31,218 +31,242 @@ std::string List::to_string(Operator op) const {
 }
 
 int List::get_total_dept_cnt() const {
-    int cnt = 0;
+    int cnt = 0; // count the number of departments
     for (int i = 0; i < MAX_DEPT; i++)
-        cnt += min(dept_status[i].cnt, 1); // 0 or 1
+        cnt += min(dept_status[i].cnt, 1); // cnt += (0 or 1 <- if the number of students in the department is 0, it is not counted)
     return cnt;
 }
 
 bool List::add(Node *node) {
-    if (node->data < *this) return false;
+    if (node->data < *this) return false; // if the student is already in the list, return false
     
-    std::string dept = node->data.get_dept();
-    int index = -1;
-    bool full = get_total_dept_cnt() == MAX_DEPT;
-    for (int i = 0; i < MAX_DEPT; i++) {
+    // check if the department is full
+    std::string dept = node->data.get_dept(); // get the department of the student to be added
+    int index = -1; // index of the department in the array (negative index indicates that the node cannot be added to the list)
+    bool full = get_total_dept_cnt() == MAX_DEPT; // check if the department is full
+    for (int i = 0; i < MAX_DEPT; i++) { // iterate throughout the array
+        // In the case of full department, check if the department is already in the array so that we can just increment the number of students in the department.
+        // In the case of not full department, check if the department is not in the array so that we can add the department to the array. 
         if ((full && dept_status[i].dept == dept) || (!full && dept_status[i].dept == "")) {
-            index = i;
-            break;
+            index = i; // save the index where the node can be inserted/added to the list
+            break; // stop the loop
         }
     }
 
-    if (index < 0) return false;
-    if (!full)
-        dept_status[index].dept = dept;
-    dept_status[index].cnt++;
+    if (index < 0) return false; // the node cannot be added
+    if (!full) // if the node can be added and the department is not full
+        dept_status[index].dept = dept; // insert the department to the array
+    dept_status[index].cnt++; // increment the number of students in the department by 1
 
-    Node *prev = head;
-    while (prev != nullptr) {
-        if (prev->next == nullptr) {
-            prev->next = node;
-            break;
+    Node *prev = head; // start from the dummy node (to add the first node easily (head->next))
+    while (prev != nullptr) { // iterate throughout the list
+        if (prev->next == nullptr) { // if the current node is the last node
+            prev->next = node; // add the node at the end of the list
+            break; // stop the loop
         }
-        prev = prev->next;
+        prev = prev->next; // move on to the next node
     }
     sort(); // TODO: Move it to print()?
 
-    return true;
+    return true; // the node has been added successfully
 }
 
 bool List::remove(Node *node) {
-    if (!(node->data < *this)) return false;
+    if (!(node->data < *this)) return false; // if the student is not in the list, return false
     
-    std::string dept = node->data.get_dept();
+    std::string dept = node->data.get_dept(); // get the department of the student to be removed
 
-    Node *prev = head;
-    while (prev != nullptr) {
-        if (prev->next->data == node->data) {
-            prev->next = prev->next->next;
-            delete node;
-            break;
+    Node *prev = head; // start from the dummy node (to remove the first node easily (head->next))
+    while (prev != nullptr) { // iterate throughout the list
+        if (prev->next->data == node->data) { // if the node to be removed is found
+            prev->next = prev->next->next; // remove the node
+            delete node; // free the memory of the node
+            break; // stop the loop
         }
-        prev = prev->next;
+        prev = prev->next; // move on to the next node
     }
 
+    // update the array of departments
     for (int i = 0; i < MAX_DEPT; i++) {
-        if (dept_status[i].dept == dept) {
-            if (--dept_status[i].cnt == 0) dept_status[i].dept = "";
+        if (dept_status[i].dept == dept) { // if the department is in the array already
+            if (--dept_status[i].cnt == 0) dept_status[i].dept = ""; // decrease the number of students in the department by 1
+            // If after decreasing the number of students it is 0, then it means there is no student in the department any more. 
+            // So remove the department from the array by setting the department name to an empty string.
             break;
         }
     }
 
-    return true;
+    return true; // the node has been removed successfully
 }
 
 void List::print() const {
-    Node *cur = head->next;
+    Node *cur = head->next; // start from the first node
     while (cur != nullptr) {
-        Student s = cur->data;
-        std::cout << s;
-        cur = cur->next;
+        Student s = cur->data; // get the student data
+        std::cout << s; // print the student data
+        cur = cur->next; // move on to the next node
     }
 }
 
 void List::sort() { // sort algorithm: selection sort
-    Node *cur = head->next;
-    while (cur != nullptr) {
-        Node *min = cur;
-        Node *tmp = cur->next;
-        while (tmp != nullptr) {
-            if (tmp->data < min->data)
-                min = tmp;
-            tmp = tmp->next;
+    Node *cur = head->next; // start from the first node
+    while (cur != nullptr) { // iterate throughout the list
+        Node *min = cur; // mark the current node as the minimum node (temporarily)
+        Node *tmp = cur->next; // temporary cursor for nested loop
+        while (tmp != nullptr) { // iterate throughout the list
+            if (tmp->data < min->data) // if the current node is smaller than the node marked as the minimum node
+                min = tmp; // mark the current node as the minimum node
+            tmp = tmp->next; // move on to the next node
         }
-        swap_node(cur, min);
-        cur = cur->next;
+        swap_node(cur, min); // swap the current node and the node marked as the minimum node
+        cur = cur->next; // move on to the next node
     }
 }
 
 void List::sort(Comp comp) {
-    Node *cur = head->next;
-    while (cur != nullptr) {
-        Node *min = cur;
-        Node *tmp = cur->next;
-        while (tmp != nullptr) {
-            if (!comp(tmp->data, min->data))
-                min = tmp;
-            tmp = tmp->next;
+    Node *cur = head->next; // start from the first node
+    while (cur != nullptr) { // iterate throughout the list
+        Node *min = cur; // mark the current node as the minimum node (temporarily)
+        Node *tmp = cur->next; // temporary cursor for nested loop
+        while (tmp != nullptr) { // iterate throughout the list
+            if (!comp(tmp->data, min->data)) // if the current node is smaller than the node marked as the minimum node (compare them using the manually defined comparison function)
+                min = tmp; // mark the current node as the minimum node
+            tmp = tmp->next; // move on to the next node
         }
-        swap_node(cur, min);
-        cur = cur->next;
+        swap_node(cur, min); // swap the current node and the node marked as the minimum node
+        cur = cur->next; // move on to the next node
     }
 }
 
 void List::pivot_dept(Operator op) {
-    sort(comp_dept);
+    sort(comp_dept); // sort the list by department in ascending order
 
-    Pivot pivots[MAX_DEPT];
-    std::string depts[MAX_DEPT];
-    int num_of_pivots = 0;
+    Pivot pivots[MAX_DEPT]; // array to store the pivots
+    std::string depts[MAX_DEPT]; // array to store the departments
+    int num_of_pivots = 0; // the number of stored pivot data
 
+    // initialize the pivot data
     for (int i = 0; i < MAX_DEPT; i++) {
-        pivots[i] = {0, MAX_AGE, 0, 0};
+        // TODO: Change the max intialized value to the minimum age of the students
+        pivots[i] = {0, MAX_AGE, 0, 0}; // initialize the pivot data
+        // Note that min is initialized to the maximum age and max is initialized to the minimum age.
     }
 
-    Node *cur = head->next;
-    while (cur != nullptr) {
-        Student s = cur->data;
+    Node *cur = head->next; // start from the first node
+    while (cur != nullptr) { // iterate throughout the list
+        Student s = cur->data; // get the student data
         if (num_of_pivots - 1 < 0 || depts[num_of_pivots - 1] != s.get_dept()) {
-            depts[num_of_pivots++] = s.get_dept();
+            // If it is the first iteration or the department has changed (next department has emerged)
+            depts[num_of_pivots++] = s.get_dept(); // add the department to the array (move on to the next department)
         }
         
-        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age());
-        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age());
-        pivots[num_of_pivots - 1].sum += s.get_age();
-        pivots[num_of_pivots - 1].cnt++;
+        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age()); // compute the minimum age in the current department
+        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age()); // compute the maximum age in the current department
+        pivots[num_of_pivots - 1].sum += s.get_age(); // compute the sum of ages in the current department (accumulate)
+        pivots[num_of_pivots - 1].cnt++; // increase the number of students in the current department by 1
         
-        cur = cur->next;
+        cur = cur->next; // move on to the next node
     }
 
     std::cout << "Dept\t" << to_string(op) << std::endl;
     for (int i = 0; i < num_of_pivots; i++) {
         if (pivots[i].sum == 0) continue; // There is no student in this pivot. 
-        std::cout << depts[i] << "\t";
-        switch (op) {
+        std::cout << depts[i] << "\t"; // print the current department
+        switch (op) { // print the result according to the operator
+            // Note that + operator is overloaded to print the double value properly (decimal place at most 1)
             case AVG: std::cout + (double) pivots[i].sum / pivots[i].cnt << std::endl; break;
             case MAX: std::cout << pivots[i].max << std::endl; break;
             case MIN: std::cout << pivots[i].min << std::endl; break;
         }
     }
 
-    sort();
+    sort(); // sort the list by the original order (dept > gender > name > age)
 }
 
 void List::pivot_gender(Operator op) {
-    sort(comp_gender);
+    sort(comp_gender); // sort the list by department in ascending order
 
-    Pivot pivots[MAX_GENDER];
-    std::string genders[MAX_GENDER];
-    int num_of_pivots = 0;
+    Pivot pivots[MAX_GENDER]; // array to store the pivots
+    std::string genders[MAX_GENDER]; // array to store the departments
+    int num_of_pivots = 0; // the number of stored pivot data
 
+    // initialize the pivot data
     for (int i = 0; i < MAX_GENDER; i++) {
-        pivots[i] = {0, MAX_AGE, 0, 0};
+        // TODO: Change the max intialized value to the minimum age of the students
+        pivots[i] = {0, MAX_AGE, 0, 0}; // initialize the pivot data
+        // Note that min is initialized to the maximum age and max is initialized to the minimum age.
     }
 
-    Node *cur = head->next;
-    while (cur != nullptr) {
-        Student s = cur->data;
+    Node *cur = head->next; // start from the first node
+    while (cur != nullptr) { // iterate throughout the list
+        Student s = cur->data; // get the student data
         if (num_of_pivots - 1 < 0 || genders[num_of_pivots - 1] != s.get_gender()) {
-            genders[num_of_pivots++] = s.get_gender();
+            // If it is the first iteration or the gender has changed (next gender has emerged)
+            genders[num_of_pivots++] = s.get_gender(); // add the gender to the array (move on to the next gender)
         }
         
-        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age());
-        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age());
-        pivots[num_of_pivots - 1].sum += s.get_age();
-        pivots[num_of_pivots - 1].cnt++;
+        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age()); // compute the minimum age in the current gender
+        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age()); // compute the maximum age in the current gender
+        pivots[num_of_pivots - 1].sum += s.get_age(); // compute the sum of ages in the current gender (accumulate)
+        pivots[num_of_pivots - 1].cnt++; // increase the number of students in the current gender by 1
         
-        cur = cur->next;
+        cur = cur->next; // move on to the next node
     }
 
     std::cout << "Gender\t" << to_string(op) << std::endl;
     for (int i = 0; i < num_of_pivots; i++) {
         if (pivots[i].sum == 0) continue; // There is no student in this pivot. 
-        std::cout << genders[i] << "\t";
-        switch (op) {
+        std::cout << genders[i] << "\t"; // print the current gender
+        switch (op) { // print the result according to the operator
+            // Note that + operator is overloaded to print the double value properly (decimal place at most 1)
             case AVG: std::cout + (double) pivots[i].sum / pivots[i].cnt << std::endl; break;
             case MAX: std::cout << pivots[i].max << std::endl; break;
             case MIN: std::cout << pivots[i].min << std::endl; break;
         }
     }
 
-    sort();
+    sort(); // sort the list by the original order (dept > gender > name > age)
 }
 
 void List::pivot_dept_gender(Operator op) {
-    Pivot pivots[MAX_DEPT_GENDER];
-    std::string depts[MAX_DEPT_GENDER];
-    std::string genders[MAX_DEPT_GENDER];
-    int num_of_pivots = 0;
+    // In this case, additional sort is not needed because the list is already sorted by dept > gender. 
 
+    Pivot pivots[MAX_DEPT_GENDER]; // array to store the pivots
+    std::string depts[MAX_DEPT_GENDER]; // array to store the departments
+    std::string genders[MAX_DEPT_GENDER]; // array to store the genders
+    int num_of_pivots = 0; // the number of stored pivot data
+
+    // initialize the pivot data
     for (int i = 0; i < MAX_DEPT_GENDER; i++) {
+        // TODO: Change the max intialized value to the minimum age of the students
         pivots[i] = {0, MAX_AGE, 0, 0};
+        // Note that min is initialized to the maximum age and max is initialized to the minimum age.
     }
 
-    Node *cur = head->next;
-    while (cur != nullptr) {
-        Student s = cur->data;
+    Node *cur = head->next; // start from the first node
+    while (cur != nullptr) { // iterate throughout the list
+        Student s = cur->data; // get the student data
         if (num_of_pivots - 1 < 0 || depts[num_of_pivots - 1] != s.get_dept() || genders[num_of_pivots - 1] != s.get_gender()) {
-            depts[num_of_pivots] = s.get_dept();
-            genders[num_of_pivots++] = s.get_gender();
+            // If it is the first iteration or the department/gender has changed (next department/gender has emerged)
+            depts[num_of_pivots] = s.get_dept(); // add the department to the array
+            genders[num_of_pivots++] = s.get_gender(); // add the gender to the array
+            // Move on to the next department and gender
         }
         
-        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age());
-        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age());
-        pivots[num_of_pivots - 1].sum += s.get_age();
-        pivots[num_of_pivots - 1].cnt++;
+        pivots[num_of_pivots - 1].min = min(pivots[num_of_pivots - 1].min, s.get_age()); // compute the minimum age in the current pivot (department & gender)
+        pivots[num_of_pivots - 1].max = max(pivots[num_of_pivots - 1].max, s.get_age()); // compute the maximum age in the current pivot (department & gender)
+        pivots[num_of_pivots - 1].sum += s.get_age(); // compute the sum of ages in the current pivot (department & gender) (accumulate)
+        pivots[num_of_pivots - 1].cnt++; // increase the number of students in the current pivot (department & gender) by 1
         
-        cur = cur->next;
+        cur = cur->next; // move on to the next node
     }
 
     std::cout << "Dept\tGender\t" << to_string(op) << std::endl;
     for (int i = 0; i < num_of_pivots; i++) {
         if (pivots[i].sum == 0) continue; // There is no student in this pivot. 
-        std::cout << depts[i] << "\t" << genders[i] << "\t";
-        switch (op) {
+        std::cout << depts[i] << "\t" << genders[i] << "\t"; // print the current department and gender
+        switch (op) { // print the result according to the operator
+            // Note that + operator is overloaded to print the double value properly (decimal place at most 1)
             case AVG: std::cout + (double) pivots[i].sum / pivots[i].cnt << std::endl; break;
             case MAX: std::cout << pivots[i].max << std::endl; break;
             case MIN: std::cout << pivots[i].min << std::endl; break;
@@ -251,11 +275,11 @@ void List::pivot_dept_gender(Operator op) {
 }
 
 bool operator<(const Student &s, const List &list) {
-    Node *cur = list.head->next;
-    while (cur != nullptr) {
-        if (s == cur->data)
+    Node *cur = list.head->next; // Set the cursor to the first node (skip the dummy node)
+    while (cur != nullptr) { // While the cursor is not pointing to the last node (repeat until the end)
+        if (s == cur->data) // if the target is found
             return true;
-        cur = cur->next;
+        cur = cur->next; // move on to the next node
     }
-    return false;
+    return false; // the target has not been found
 }
--- assn2/src/list.hpp
+++ assn2/src/list.hpp
@@ -1,75 +1,152 @@
 /*
  * CSED232 ASSN2
  * Author: Yeho Gwon (20220358)
- * Date: 2023/03/05
+ * Date: 2023/03/06
  * File Name: list.hpp
  */
 
 #ifndef __LIST_HPP__
 #define __LIST_HPP__
 
-#include <iostream> // Only for pivot table functions
+#include <iostream> // Only for pivot table functions (print the results)
 #include <string>
 #include "student.hpp"
 #include "node.hpp"
 #include "io.hpp"
 
-#define MAX_AGE 30
-#define MAX_DEPT 9
-#define MAX_GENDER 2
+#define MAX_AGE 30 // maximum age of students
+#define MAX_DEPT 9 // maximum number of departments
+#define MAX_GENDER 2 // the number of types of gender ("M" / "F")
 #define MAX_DEPT_GENDER (MAX_DEPT * MAX_GENDER)
 
-typedef bool (*Comp)(const Student &s1, const Student &s2);
+typedef bool (*Comp)(const Student &s1, const Student &s2); // function pointer type (Student, Student) -> bool
 
-enum Operator { AVG = 1, MAX = 2, MIN = 3 };
+enum Operator { AVG = 1, MAX = 2, MIN = 3 }; // enum type for indiciating a pivot table operator
 
 class List {
 private: 
-    struct Pivot {
+    struct Pivot { // struct to store the result of pivot table (accumulate)
         int max, min, cnt, sum;
     };
 
-    struct DeptStatus {
+    struct DeptStatus { // struct to store the status of departments (the name of the department and the number of students in the department)
         std::string dept;
         int cnt;
     };
 
-    Node *head; // the first node is a dummy node
+    Node *head; // head node; used as a dummy node
 
-    DeptStatus dept_status[MAX_DEPT];
+    DeptStatus dept_status[MAX_DEPT]; // store the departments as an array (the name of each department and the number of students in the department)
 
-    std::string to_string(Operator op) const;
+    /**
+     * @brief convert the given operator to string (the name of the operator)
+     * @param op operator to be converted
+     * @return name (Average, Max, Min) of the operator
+     */
+    std::string to_string(Operator op) const; // TODO: move this method outside the class
+    /**
+     * @brief return the number of departments in the list
+     * @return the total number of departments in the list
+     */
     int get_total_dept_cnt() const;
 
 public: 
+    /**
+     * @brief constructor
+     */
     List();
+    /**
+     * @brief destructor
+     */
     ~List();
 
+    /**
+     * @brief add a node to the list
+     * @param node node to be added
+     * @return true if the node is added successfully, false otherwise
+     */
     bool add(Node *node);
+
+    /**
+     * @brief remove a node from the list
+     * @param node node to be removed
+     * @return true if the node is removed successfully, false otherwise
+     */
     bool remove(Node *node);
 
+    /**
+     * @brief print the list
+     * @return none
+     */
     void print() const;
-    
+
+    /**
+     * @brief sort the list following the instruction using selection sort algorithm (dept > gender > name > age; ascending order)
+     * @return none
+     */
     void sort();
+
+    /**
+     * @brief sort the list following the given comparison function using selection sort algorithm
+     * @param comp comparison function (function pointer that gets two Student objects and returns the order in bool type)
+     * @return none
+     */
     void sort(Comp comp);
 
+    /**
+     * @brief conduct pivot table with dept as a pivot variable with the given operator and print the result
+     * @param op operator to conduct pivot table with
+     * @return none
+     */
     void pivot_dept(Operator op);
+
+    /**
+     * @brief conduct pivot table with gender as a pivot variable with the given operator and print the result
+     * @param op operator to conduct pivot table with
+     * @return none
+     */
     void pivot_gender(Operator op);
+
+    /**
+     * @brief conduct pivot table with dept and gender as a pivot variable with the given operator and print the result
+     * @param op operator to conduct pivot table with
+     * @return none
+     */
     void pivot_dept_gender(Operator op);
 
     friend bool operator<(const Student &s, const List &list);
 };
 
+/**
+ * @brief find the maximum value of two arguments
+ * @tparam T type of the two arguments and return value
+ * @param a first argument
+ * @param b second argument
+ * @return the maximum value of the two arguments
+ */
 template <typename T>
 T max(T a, T b) {
     return a > b ? a : b;
 }
 
+/**
+ * @brief find the minimum value of two arguments
+ * @tparam T type of the two arguments and return value
+ * @param a first argument
+ * @param b second argument
+ * @return the minimum value of the two arguments
+ */
 template <typename T>
 T min(T a, T b) {
     return a < b ? a : b;
 }
 
+/**
+ * @brief check if the Student object (s) is in the list (list) (operator overloading)
+ * @param s Student object to be checked
+ * @param list list to be checked from
+ * @return true if s is in the list, false otherwise
+ */
 bool operator<(const Student &s, const List &list); // Check if the student exists in the list
 
 #endif
