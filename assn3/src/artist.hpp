b759ca6fb0706cce6f30e74bce033025da522ecf
Yeho Gwon <devstita@gmail.com>
2023-04-15 23:48:43 +0900

    implemented artist

--- assn3/src/artist.hpp
+++ assn3/src/artist.hpp
@@ -3,8 +3,19 @@
 
 #include <vector>
 
+const char TOKENS[] = {'@', '&', '%', 'W', 'X', 'A', 'H', 'O', 'T', '*', '^', '+', '-', '.', ' '};
+const int THRESHOLD = 50;
+
 // style of ASCII art
 class artist {
+private: 
+    int width;
+    int height;
+    std::vector<int> pixels;
+
+protected: 
+    int get_pixel(int x, int y);
+
 public: 
     /**
      * @brief A constructor for the artist class
@@ -26,6 +37,14 @@ public:
 class classic : public artist {
 public:
     /**
+     * @brief A constructor for the classic class
+     * @param width The width of the image
+     * @param height The height of the image
+     * @param pixels The pixel data of the image
+    */
+    classic(int width, int height, const std::vector<int> &pixels);
+
+    /**
      * @brief convert pixel to ASCII character in a classic style
      * @param x x-coordinate of pixel
      * @param y y-coordinate of pixel
@@ -36,6 +55,14 @@ public:
 
 class iclassic : public artist {
 public:
+    /**
+     * @brief A constructor for the iclassic class
+     * @param width The width of the image
+     * @param height The height of the image
+     * @param pixels The pixel data of the image
+    */
+    iclassic(int width, int height, const std::vector<int> &pixels);
+
     /**
      * @brief convert pixel to ASCII character in an inverted classic style
      * @param x x-coordinate of pixel
@@ -48,6 +75,14 @@ public:
 class sobelx : public artist {
 public:
     /**
+     * @brief A constructor for the soblex class
+     * @param width The width of the image
+     * @param height The height of the image
+     * @param pixels The pixel data of the image
+    */
+    sobelx(int width, int height, const std::vector<int> &pixels);
+
+    /**
      * @brief convert pixel to ASCII character in a horizontal-edge-detection style
      * @param x x-coordinate of pixel
      * @param y y-coordinate of pixel
@@ -59,6 +94,14 @@ public:
 class sobely : public artist {
 public:
     /**
+     * @brief A constructor for the sobely class
+     * @param width The width of the image
+     * @param height The height of the image
+     * @param pixels The pixel data of the image
+    */
+    sobely(int width, int height, const std::vector<int> &pixels);
+
+    /**
      * @brief convert pixel to ASCII character in a vertial-edge-detection style
      * @param x x-coordinate of pixel
      * @param y y-coordinate of pixel
@@ -69,6 +112,14 @@ public:
 
 class gradient : public artist {
 public:
+    /**
+     * @brief A constructor for the gradient class
+     * @param width The width of the image
+     * @param height The height of the image
+     * @param pixels The pixel data of the image
+    */
+    gradient(int width, int height, const std::vector<int> &pixels);
+
     /**
      * @brief convert pixel to ASCII character in a gradient style (sobelx + sobely)
      * @param x x-coordinate of pixel
