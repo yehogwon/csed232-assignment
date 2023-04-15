821a46c6394aee4b2f6a4f7c14b6ffa24ab77762
Yeho Gwon <devstita@gmail.com>
2023-04-15 23:13:16 +0900

    implemented parser

---
+++ assn3/src/parser.cpp
@@ -0,0 +1,29 @@
+#include "parser.hpp"
+
+std::vector<int> parser::load_image(const char *input_file) {
+    std::ifstream in_file(input_file);
+    if (!in_file.is_open()) {
+        // EXP: Unable to open file
+    }
+
+    std::string _data;
+    getline(in_file, _data);
+    return split<int>(_data, SEP, [](const std::string &s) {return std::stoi(s);});
+}
+
+std::vector<std::string> parser::load_config(const char *input_file) {
+    std::ifstream in_file(input_file);
+    if (!in_file.is_open()) {
+        // EXP: Unable to open file
+    }
+
+    std::string _data;
+    getline(in_file, _data);
+    return split<std::string>(_data, SEP);
+}
+
+void parser::write_result(const char *output_file, const std::string &result) {
+    std::ofstream out_file(output_file);
+    out_file << result;
+    out_file.close();
+}
--- assn3/src/parser.hpp
+++ assn3/src/parser.hpp
@@ -2,10 +2,28 @@
 #define __PARSER_HPP__
 
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
+
+const char SEP = '|';
 
 class parser {
+private: 
+    template <typename T>
+    using cast = T (*)(const std::string&);
+
+    /**
+     * @brief split given string by given delimiter
+     * @tparam T type of each separated value
+     * @param s string to be split
+     * @param delimiter delimiter
+     * @param cast_ function to cast string to T
+     * @return std::vector of T each of whose elements is separated by given delimiter
+    */
+    template <typename T>
+    std::vector<T> split(const std::string &s, const char delimiter, const cast<T> cast_ = nullptr) const;
 public: 
     /**
      * @brief Load image from file
@@ -28,5 +46,25 @@ public:
     */
     void write_result(const char *output_file, const std::string &result);
 };
+
+template <typename T>
+std::vector<T> parser::split(const std::string &s, const char delimiter, const cast<T> cast_) const {
+    std::vector<std::string> v_ = split<std::string>(s, delimiter, nullptr);
+    std::vector<T> v;
+    for (const std::string &s_ : v_) v.push_back(cast_(s_));
+    return v;
+}
+
+template <>
+std::vector<std::string> parser::split<std::string>(const std::string &s, const char delimiter, const cast<std::string> cast_) const {
+    std::vector<std::string> v;
+    
+    std::istringstream iss(s);
+    std::string buf_;
+    while (getline(iss, buf_, delimiter))
+        v.push_back(buf_);
+
+    return v;
+}
 
 #endif
