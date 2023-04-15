e83f69ae0dd9d4a812d2c7775fa94edcb4963e34
Yeho Gwon <devstita@gmail.com>
2023-04-16 00:45:04 +0900

    added scope resolution std::

--- assn3/src/artist.cpp
+++ assn3/src/artist.cpp
@@ -35,7 +35,7 @@ sobelx::sobelx(int width, int height, const std::vector<int> &pixels)
 
 char sobelx::mapper(int x, int y) {
     int cur = get_pixel(x, y), right = get_pixel(x + 1, y);
-    if (right >= 0 && abs(cur - right) >= THRESHOLD) return '|';
+    if (right >= 0 && std::abs(cur - right) >= THRESHOLD) return '|';
     else return ' ';
 }
 
@@ -44,7 +44,7 @@ sobely::sobely(int width, int height, const std::vector<int> &pixels)
 
 char sobely::mapper(int x, int y) {
     int cur = get_pixel(x, y), bottom = get_pixel(x, y + 1);
-    if (bottom >= 0 && abs(cur - bottom) >= THRESHOLD) return '-';
+    if (bottom >= 0 && std::abs(cur - bottom) >= THRESHOLD) return '-';
     else return ' ';
 }
 
@@ -55,10 +55,10 @@ char gradient::mapper(int x, int y) {
     // TODO: Check if it works and make it fancier!
     int cur = get_pixel(x, y), right = get_pixel(x + 1, y), bottom = get_pixel(x, y + 1);
     if (right < 0 && bottom < 0) return ' ';
-    // if ((bottom < 0 || (right >= 0 && bottom >= 0)) && abs(cur - right) >= THRESHOLD) return '|';
-    if ((bottom < 0 || right >= 0) && abs(cur - right) >= THRESHOLD) return '|';
-    // if ((right < 0 || (right >= 0 && bottom >= 0)) && abs(cur - bottom) >= THRESHOLD) return '-';
-    if ((right < 0 || bottom >= 0) && abs(cur - bottom) >= THRESHOLD) return '-';
-    if (right >= 0 && bottom >= 0 && abs(cur - right) >= THRESHOLD && abs(cur - bottom) >= THRESHOLD) return '+';
+    // if ((bottom < 0 || (right >= 0 && bottom >= 0)) && std::abs(cur - right) >= THRESHOLD) return '|';
+    if ((bottom < 0 || right >= 0) && std::abs(cur - right) >= THRESHOLD) return '|';
+    // if ((right < 0 || (right >= 0 && bottom >= 0)) && std::abs(cur - bottom) >= THRESHOLD) return '-';
+    if ((right < 0 || bottom >= 0) && std::abs(cur - bottom) >= THRESHOLD) return '-';
+    if (right >= 0 && bottom >= 0 && std::abs(cur - right) >= THRESHOLD && std::abs(cur - bottom) >= THRESHOLD) return '+';
     return ' ';
 }
