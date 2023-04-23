85c0c4b3417195355b882470d3664342ce1de943
Yeho Gwon <devstita@gmail.com>
2023-04-23 14:25:35 +0900

    working on the comment on artist.cpp

--- assn3/src/artist.cpp
+++ assn3/src/artist.cpp
@@ -14,40 +14,40 @@ int artist::get_height() const {
 }
 
 int artist::get_pixel(int x, int y) const {
-    if (x < 0 || x >= width || y < 0 || y >= height) return -1;
-    return pixels[x + y * width];
+    if (x < 0 || x >= width || y < 0 || y >= height) return -1; // out of range
+    return pixels[x + y * width]; // return the pixel value
 }
 
 classic::classic(int width, int height, const std::vector<int> &pixels)
  : artist(width, height, pixels) { }
 
 char classic::mapper(int x, int y) const {
-    return TOKENS[min(get_pixel(x, y) / 17, 14)];
+    return TOKENS[min(get_pixel(x, y) / 17, 14)]; // map pixel value to a character
 }
 
 iclassic::iclassic(int width, int height, const std::vector<int> &pixels)
  : artist(width, height, pixels) { }
 
 char iclassic::mapper(int x, int y) const {
-    return TOKENS[max(0, 14 - get_pixel(x, y) / 17)];
+    return TOKENS[max(0, 14 - get_pixel(x, y) / 17)]; // map pixel value to a character (reversed order)
 }
 
 sobelx::sobelx(int width, int height, const std::vector<int> &pixels)
  : artist(width, height, pixels) { }
 
 char sobelx::mapper(int x, int y) const {
-    int cur = get_pixel(x, y), right = get_pixel(x + 1, y);
-    if (right >= 0 && abs(cur - right) >= THRESHOLD) return '|';
-    else return ' ';
+    int cur = get_pixel(x, y), right = get_pixel(x + 1, y); // current pixel and right adjacent pixel
+    if (right >= 0 && abs(cur - right) >= THRESHOLD) return '|'; // if the difference is large enough (THRESHOLD), return '|' (only when the right adjacent pixel is in range)
+    else return ' '; // otherwise return ' '
 }
 
 sobely::sobely(int width, int height, const std::vector<int> &pixels)
  : artist(width, height, pixels) { }
 
 char sobely::mapper(int x, int y) const {
-    int cur = get_pixel(x, y), bottom = get_pixel(x, y + 1);
-    if (bottom >= 0 && abs(cur - bottom) >= THRESHOLD) return '-';
-    else return ' ';
+    int cur = get_pixel(x, y), bottom = get_pixel(x, y + 1); // current pixel and bottom adjacent pixel
+    if (bottom >= 0 && abs(cur - bottom) >= THRESHOLD) return '-'; // if the difference is large enough (THRESHOLD), return '-' (only when the bottom adjacent pixel is in range)
+    else return ' '; // otherwise return ' '
 }
 
 gradient::gradient(int width, int height, const std::vector<int> &pixels)
@@ -66,5 +66,5 @@ digit::digit(int width, int height, const std::vector<int> &pixels)
  : artist(width, height, pixels) { }
 
 char digit::mapper(int x, int y) const {
-    return DIGITS[min(get_pixel(x, y) / 25, 9)];
+    return DIGITS[min(get_pixel(x, y) / 25, 9)]; // map pixel value to a character (0-9 decimal digits)
 }
