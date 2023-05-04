716561e7b2b3871345862f7d47fef428c583a6b8
Yeho Gwon <devstita@gmail.com>
2023-05-05 01:04:18 +0900

    constructed ctest for assn2

---
+++ assn2/tests/node.test.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+
+#include "node.hpp"
+
+bool equal_test() {
+    Student a, b;
+    Node n1(a), n2(b);
+    return !(n1 == n2);
+}
+
+bool swap_test() {
+    Student a, b;
+    Node n3(a), n4(b);
+    Node n1(a, &n3), n2(b, &n4);
+    swap_node(&n1, &n2);
+
+    Node n5(a), n6(b, &n5);
+    swap_node(&n5, &n6);
+    
+    return n1.next == &n4 && n2.next == &n3 && n5.next == &n6 && n6.next == nullptr;
+}
+
+int main() {
+    bool t1 = equal_test();
+    bool t2 = swap_test();
+    return !(t1 && t2);
+}
