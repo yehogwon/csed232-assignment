e02631817b0499cd21b5f1f377b05b85e13e2b2e
Yeho Gwon <devstita@gmail.com>
2023-05-05 13:54:40 +0900

    implemented assn2 test in an iterative manner

--- assn2/tests/student.test.cpp
+++ assn2/tests/student.test.cpp
@@ -1,12 +1,29 @@
 #include "student.hpp"
+#include <string>
 #include <sstream>
+#include <tuple>
+// #include <utility>
 
 bool input_test() {
-    std::istringstream iss("CSE\nM\nKim\n23\n");
-    std::ostringstream oss;
-    Student s(iss, oss);
-    bool check1 = s.get_dept() == "CSE" && s.get_gender() == "M" && s.get_age() == 23;
-    return check1;
+    std::pair<const char*, std::tuple<const char*, const char*, int>> proper_cases[] {
+        std::make_pair("CSE\nM\nKim\n23\n", std::make_tuple("CSE", "M", 23)), 
+        std::make_pair("BIO\nF\nNana\n29\n", std::make_tuple("BIO", "F", 29))
+    };
+
+    bool check = true;
+    for (auto test_case : proper_cases) {
+        if (!check) break;
+        std::istringstream iss(test_case.first);
+        std::ostringstream oss;
+        Student s(iss, oss);
+        check = check && 
+                s.get_dept() == std::get<0>(test_case.second) && 
+                s.get_gender() == std::get<1>(test_case.second) && 
+                s.get_age() == std::get<2>(test_case.second);
+    }
+    if (!check) return check;
+    
+    return check;
 }
 
 bool output_test() {
