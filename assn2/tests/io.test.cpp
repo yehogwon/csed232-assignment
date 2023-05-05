ff04ac316ffe53501f7e89f5a342fc52efb38981
Yeho Gwon <devstita@gmail.com>
2023-05-05 17:21:24 +0900

    structured io.test for assn2

--- assn2/tests/io.test.cpp
+++ assn2/tests/io.test.cpp
@@ -1,3 +1,21 @@
-int main() {
-    return 0;
+#include "io.hpp"
+#include "test.h"
+#include <sstream>
+
+bool strict_test() {
+    return true;
+}
+
+bool range_test() {
+    return true;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) return 1; // invalid arguments (requires test name)
+
+    std::vector<std::pair<std::string, fp>> tests {
+        std::make_pair("Format::strict_input", strict_test), 
+        std::make_pair("Format::range_input", range_test)
+    };
+    return test(argv[1], tests);
 }
