7d4d0efa9f9b93aa47b771ee7c08f0189268a5e6
Yeho Gwon <devstita@gmail.com>
2023-05-07 14:51:39 +0900

    implemented basic sharedptr test

--- assn4/tests/sharedptr.test.cpp
+++ assn4/tests/sharedptr.test.cpp
@@ -1,16 +1,38 @@
+#define _TEST_COUT__
+#include <iostream>
+#include <sstream>
 #include "test.h"
 #include "SharedPtr.h"
-#include <sstream>
+
+class MyClass {
+public: 
+    MyClass() {
+        std::cout << "CONSTRUCTOR" << std::endl;
+    }
+
+    ~MyClass() {
+        std::cout << "DESTRUCTOR" << std::endl;
+    }
+};
 
 bool sharedptr_test() {
-    /* Code Snippet for Catching std::cout */
-    std::stringstream buffer;
-    std::streambuf *sbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
-    // print something here
-    std::cout.rdbuf(sbuf);
+    std::vector<std::string> ans_set = {
+        "CONSTRUCTOR", 
+        "Dealloc Object", 
+        "DESTRUCTOR"
+    };
 
-    return true;
+    START_TEST_COUT__(cout_)
+    {
+        SharedPtr<MyClass> ptr1(new MyClass());
+        SharedPtr<MyClass> ptr2(ptr1);
+    }
+    STOP_TEST_COUT__
+
+    std::string ans;
+    for (const auto &s : ans_set) ans += s + "\n";
+    
+    return cout_.str() == ans;
 }
 
 int main(int argc, char **argv) {
