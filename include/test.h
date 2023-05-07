c4bea113abd32ee4a74b97b7ce881cd1adb327c1
Yeho Gwon <devstita@gmail.com>
2023-05-07 14:51:50 +0900

    added a feature to print testcase

--- include/test.h
+++ include/test.h
@@ -17,6 +17,12 @@
 
 using fp = bool (*)();
 
+#ifdef _GLIBCXX_IOSTREAM
+void show_case(std::string res, std::string ans) {
+    std::cout << "Got: '''\n" << cout_.str() << "\n'''\nExpected: '''" << ans << "\n'''" << std::endl;
+}
+#endif
+
 int test(char *test_name, const std::vector<std::pair<std::string, fp>> &tests) {
     for (const auto &test : tests) {
         if (test.first != test_name) continue;
