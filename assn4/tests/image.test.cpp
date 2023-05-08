06c7ee4be22f6b7c40fa2a4d428d04c553523915
Yeho Gwon <devstita@gmail.com>
2023-05-08 17:39:22 +0900

    used template to test on both types

--- assn4/tests/image.test.cpp
+++ assn4/tests/image.test.cpp
@@ -14,24 +14,37 @@ Image<RGBf> create_image_f() {
     return img;
 }
 
+template<typename T>
+Image<T> create_image() {
+    throw std::invalid_argument("Invalid pixel type");
+}
+
+template <>
+Image<RGB8b> create_image() {
+    return create_image_b();
+}
+
+template <>
+Image<RGBf> create_image() {
+    return create_image_f();
+}
+
 bool construct_test() {
     return true;
 }
 
-bool assign_test() {
-    Image<RGB8b> imgb = create_image_b();
-    Image<RGB8b> imgb_ = imgb;
-    if (imgb.width() != imgb_.width() || imgb.height() != imgb_.height()) return false;
-    for (int i = 0; i < imgb.width() * imgb.height(); i++)
-        if (imgb[i] != imgb_[i]) return false;
-        
-    Image<RGBf> imgf = create_image_f();
-    Image<RGBf> imgf_ = imgf;
-    if (imgf.width() != imgf_.width() || imgf.height() != imgf_.height()) return false;
-    for (int i = 0; i < imgf.width() * imgf.height(); i++)
-        if (imgf[i] != imgf_[i]) return false;
-    
+template <typename T>
+bool assign_test_() {
+    Image<T> img = create_image<T>();
+    Image<T> img_ = img;
+    if (img.width() != img_.width() || img.height() != img_.height()) return false;
+    for (int i = 0; i < img.width() * img.height(); i++)
+        if (img[i] != img_[i]) return false;
     return true;
+}
+
+bool assign_test() {
+    return assign_test_<RGB8b>() && assign_test_<RGBf>();
 }
 
 bool memory_test() {
