56db0cad0ba69c952efa00a3514d6650439b2bde
Yeho Gwon <devstita@gmail.com>
2023-05-22 21:12:03 +0900

    remove callback

--- assn5/src/game/game.hpp
+++ assn5/src/game/game.hpp
@@ -2,23 +2,18 @@
 #define __GAME__
 
 #include <array>
-#include <vector>
 #include "block.hpp"
 #include "board.hpp"
 
 class Game {
-    using callback_ = void (*)();
 private: 
     Board *prev_board_;
     Board *board_;
-    std::vector<callback_> callback_v_;
 
     bool create_block();
 public: 
     Game();
     ~Game();
-
-    void add_update_callback(callback_ callback);
 
     const Board& prev() const;
     const Board& cur() const;
@@ -27,7 +22,7 @@ public:
     bool right();
     bool up();
     bool down();
-    
+
     bool is_game_over();
 
     std::array<Block, SIZE>& operator[](int i);
