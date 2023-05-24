1bb93b6216749889df49060bda822f2386ffc452
Yeho Gwon <devstita@gmail.com>
2023-05-24 14:18:32 +0900

    test on instruction

--- assn4/tests/sharedptr.test.cpp
+++ assn4/tests/sharedptr.test.cpp
@@ -208,6 +208,7 @@ int main(int argc, char **argv) {
     if (argc != 2) return 1; // invalid arguments (requires test name)
 
     std::vector<std::pair<std::string, fp>> tests {
+        std::make_pair("SharedPtr::SharedPtrManual", __sharedptr_test), 
         std::make_pair("SharedPtr::SharedPtr", sharedptr_test)
     };
     return test(argv[1], tests);
